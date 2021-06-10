e9654626da8b54ea3bc30a5875ae0d349009010e
blackorbit1 <enzo.dutra@gmail.com>
2021-06-10 21:41:12 +0200

    ajout de méthodes fréquemment utilisées en JS

---
+++ public/js/common.js
@@ -0,0 +1,33 @@
+// ---------- requetes HTTP ---------- //
+
+function httpGet(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+    return xhr.responseText;
+}
+
+// inspired from : https://stackoverflow.com/questions/3748929/passing-parameter-to-a-javascript-function-as-a-dictionary-object
+function getArgsBuilder(object) {
+    var getArgs = [];
+    for (var key in object) if (object.hasOwnProperty(key)) {
+        getArgs.push(encodeURIComponent(key) + '=' + encodeURIComponent(object[key]));
+    }
+    return getArgs.join('&');
+}
+
+function httpPost(url, data) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url, false);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(data);
+    return xhr.responseText;
+}
+
+
+// ---------- DOM ---------- //
+
+function getHTML(cssSelector){      return document.querySelector(cssSelector).outerHTML }
+function getInnerHTML(cssSelector){ return document.querySelector(cssSelector).innerHTML }
+function getValue(cssSelector){     return document.querySelector(cssSelector).value }
+
